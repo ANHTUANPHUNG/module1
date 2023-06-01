74b654ba3b4e55634c4c26fc02fa9d60c5be7cb5
ANHTUANPHUNG <132957644+ANHTUANPHUNG@users.noreply.github.com>
2023-06-01 10:39:35 +0700

    update

---
+++ tieman/data.js
@@ -0,0 +1,9 @@
+let listProduct = [
+    { id: 1, image: 'fileanh/product_1.png', name: "Món 1", category: 'Món chay', description: "Món 1 ngon lắm", price: 100000, rating: 5, sizes: [5, 10, 20, 30] },
+    { id: 2, image: 'fileanh/product_2.png', name: "Món 2", category: 'Món mặn', description: "Món 2 ngon lắm", price: 20000, rating: 5, sizes: [5, 10, 20, 30] },
+    { id: 3, image: 'fileanh/product_3.png', name: "Món 3", category: 'Món chay', description: "Món 3 ngon lắm", price: 5000, rating: 5, sizes: [5, 10, 20, 30] },
+    { id: 4, image: 'fileanh/product_4.png', name: "Món 4", category: 'Món mặn', description: "Món 4 ngon lắm", price: 1, rating: 5, sizes: [5, 10, 20, 30] },
+    { id: 5, image: 'fileanh/product_5.png', name: "Món 5", category: 'Món chay', description: "Món 5 ngon lắm", price: 2, rating: 5, sizes: [5, 10, 20, 30] },
+    { id: 6, image: 'fileanh/product_6.png', name: "Món 5", category: 'Món chay', description: "Món 5 ngon lắm", price: 2, rating: 5, sizes: [5, 10, 20, 30] },
+]
+
